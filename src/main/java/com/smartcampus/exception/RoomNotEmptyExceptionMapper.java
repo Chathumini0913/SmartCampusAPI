62c89347a8b97232744fd65b64aa30cb0a8985cb
Chathumini Priyasha Rathnayaka <chathumini.20241955@iit.ac.lk>
2026-04-24 03:00:49 +0530

    Add custom exception handling and mappers

---
+++ src/main/java/com/smartcampus/exception/RoomNotEmptyExceptionMapper.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.exception;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.Map;
+
+
+/**
+ *
+ * @author ASUS
+ */
+
+@Provider
+public class RoomNotEmptyExceptionMapper implements ExceptionMapper<RoomNotEmptyException> {
+    @Override
+    public Response toResponse(RoomNotEmptyException e) {
+        return Response.status(Response.Status.CONFLICT)
+                .type(MediaType.APPLICATION_JSON)
+                .entity(Map.of(
+                    "status", 409,
+                    "error", "Conflict",
+                    "message", e.getMessage()
+                )).build();
+    }
+    
+}
